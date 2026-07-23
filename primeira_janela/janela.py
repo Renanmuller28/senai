5a55ef2cf6b3f29826ac9f8e44ca40f4d2078418
PedroWeigel <renan.v.muller@aluno.senai.br>
2026-07-22 22:28:52 -0300

    Python Tkinter - Básicos

--- primeira_janela/janela.py
+++ primeira_janela/janela.py
@@ -2,6 +2,14 @@ import tkinter as tk
 
 # Cria a janela principal
 root = tk.Tk()
+root.title("SENAI - Desenvolvimento de sistemas")
+root.geometry("400x200+50+250") # Define o tamanho da janela (largura x altura + posição X + posição Y)
+
+# Cria um rótulo (label) com o texto "Hello, World!"
+message = tk.Label(root, text="Hello, World!")
+
+# Posiciona o rótulo na janela
+message.pack()
 
 # Inicia o loop principal da interface grafica
 root.mainloop()
