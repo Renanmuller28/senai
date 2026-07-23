87b695a99e2fbe4e9dcafb66536adec376b37039
PedroWeigel <renan.v.muller@aluno.senai.br>
2026-07-22 22:00:36 -0300

    Programa de filmes

--- exercicio.filmes/filmes.py
+++ exercicio.filmes/filmes.py
@@ -13,7 +13,7 @@ def main():
         elif opc == "4":
             filmes_por_genero()
         elif opc == "5":
-            media_de-duração()
+            media_duracao()
         elif opc == '6':
             print('saindo...')
             break
@@ -33,10 +33,12 @@ def menu() -> None:
 
 def contar_filmes():
     contador=0
+
     with open('filmes.txt', 'r', encoding="utf-8") as arquivo:
         for linha in arquivo:
-            if linha.strip().startswith('Título'):
-                contador=contador+1
+            if linha.strip().startswith("Título"):
+                contador =+ 1
+
     print(f'Quantidade de filmes : {contador}')
 
 def info_por_titulo():
@@ -72,8 +74,101 @@ def info_por_titulo():
                         
                         
 
-        
+def filmes_por_diretor():
+    diretor_busca=input('Diretor: ').strip().lower
+    contador = 0
+    try:
+        with open("filmes.txt", encoding="utf-8") as f:
+            ultimo_titulo=""
+            for linha in f:
+                s = linha.strip()
+                if s.startswith('Título:'):
+                    ultimo_titulo= s.split(':', 1)[1].strip()
+                elif s.startswith('Diretor:'):
+                    diretor = s.split(':',1)[1].strip()
+                    if diretor.lower() == diretor_busca:
+                        contador += 1
+                        print (f'-{ultimo_titulo}')
+    except FileNotFoundError:
+        print("Arquivo'filmes.txt não encontrado")
+        return
+    print(f"total de filmes do diretor {diretor_busca}:{contador}")
+    return contador
+
+def filmes_por_genero():
+    genero_busca=input('Gênero,').strip().lower
+    contador = 0
+    try:
+        with open("filmes.txt", encoding="utf-8") as f:
+            ultimo_titulo=""
+            for linha in f:
+                s = linha.strip()
+                if s.startswith('Título:'):
+                    ultimo_titulo= s.split(':', 1)[1].strip()
+                elif s.startwith('Gênero:'):
+                    genero = s.split(':', 1)[1].strip()
+                    if genero_busca in genero.lower() :
+                        contador += 1
+                        print (f'-{ultimo_titulo}')
+    except FileNotFoundError:
+        print("Arquivo'filmes.txt não encontrado")
+        return
+    print(f"total de filmes do diretor' {genero_busca}':{contador}")
+    return contador
+
+
+
+
+def media_duracao():
+    soma = 0
+    cont = 0
+    try:
+        with open("filmes.txt", encoding="utf-8") as f:
+            for linha in f:
+                s = linha.strip()
+                if s.startswith("Duração:"):
+                    try:
+                        minutos= int (s.split(':',1)[1].strip().split()[0])
+                    except (ValueError,IndexError):
+                        continue
+                    soma += minutos
+                    cont += 1
+    except FileNotFoundError:
+        print("Arquivo'filmes.txt'não encontrado.")
+        return
+
+    if cont== 0:
+        print("Nenhuma duração válida encontrada.")
+    else:
+        media= soma/cont
+        print(f"Média de duração: {media:.2f} minutos")
+        return media
+
+
+def adicionar_filme():
+    print("** ADICIONAR FILME **")
+    titulo = input("-> Digite o nome do filme: ").strip()
+    ano = int(input('-> Digite o ano do filme: '))
+    diretor = input('-> Digite o diretor do filme: ').strip()
+    genero = input('-> Digite o gênero do filme: ').strip()
+    duracao = int(input('-> Digite o a duração do filme: '))
+
+    try: 
+        with open("filmes.txt", "a", encoding="utf-8") as file:
+            file.write("\n")
+            file.write(f"Título: {titulo}\n")
+            file.write(f"Ano: {ano}\n")
+            file.write(f"Diretor: {diretor}\n")
+            file.write(f"Gênero: {genero}\n")
+            file.write(f"Duração: {duracao} minutos\n")
+    except FileNotFoundError:
+        print("Arquivo 'filmes.txt' não econtrado")
+
+    print("Filme adicionado com sucesso!")
+
+
+
+
+
 if __name__ == "__main__":
     main()
-
-   
